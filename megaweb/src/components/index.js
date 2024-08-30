f2c069bc7d4dfdafa7cbff8491bf12a46dfc739f
priyanshu1520 <priyanshu.thakur024@gmail.com>
2024-08-30 14:53:52 +0530

    tenth commit

--- megaweb/src/components/index.js
+++ megaweb/src/components/index.js
@@ -2,10 +2,11 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogouBtn from "./LogoutBtn";
+import LogoutBtn from "./Header/LogoutBtn";
 export{
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    LogoutBtn
 }
